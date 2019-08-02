98604a8216cb66d2b01e3335316553f18ccd99fb
rsmonroe <52563866+rsmonroe@users.noreply.github.com>
2019-08-02 06:54:28 -0400

    Update index.js
    Removed ChrismasInJuly

--- www/content/home/index.js
+++ www/content/home/index.js
@@ -1,12 +1,10 @@
 import featured from "./featured.html"
-import chrismasInJuly from "./chrismas-in-july.html"
 import announcements from "./announcements.html"
 import prairieSchooler from "./prairie-schooler.html"
 import sales from "./sales.html"
 
 export default [
   featured,
-  chrismasInJuly,
   announcements,
   prairieSchooler,
   sales,
