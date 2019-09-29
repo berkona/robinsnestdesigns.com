730153f3bef60490e40c222906a25e034b85eddf
Jon Monroe <berkona@gmail.com>
2019-09-29 13:04:03 -0700

    Fixed isActive regression

--- api/db/Product/productFields.js
+++ api/db/Product/productFields.js
@@ -1,4 +1,4 @@
-const { knex } = require('../db')
+const { knex } = require("../db");
 
 const effectivePriceCase = knex.raw(`(CASE
   WHEN Products.Price1 > 0
@@ -8,52 +8,53 @@ const effectivePriceCase = knex.raw(`(CASE
    AND Products.Sale_Stop >= CURRENT_TIMESTAMP
    THEN Products.SalePrice
   ELSE Products.ItemPrice
-  END) as EffectivePrice`)
+  END) as EffectivePrice`);
 
-const productFields =  [
-  'Products.ID as ProductID',
-  'Qty',
-  'Clearance',
-  'Products.ItemID as ItemID',
-  'Products.ItemName as ItemName',
-  'Description',
-  'Products.ItemPrice as ItemPrice',
+const productFields = [
+  "Products.ID as ProductID",
+  "Active",
+  "Qty",
+  "Clearance",
+  "Products.ItemID as ItemID",
+  "Products.ItemName as ItemName",
+  "Description",
+  "Products.ItemPrice as ItemPrice",
   effectivePriceCase,
-  'Thumbnail',
-  'Products.Image as Image',
-  'Hyperlinked_Image as hyperlinkedImage',
-  'SalePrice',
-  'Sale_Start',
-  'Sale_Stop',
-  'Price1',
-  'Price2',
-  'Price3',
-  'Price4',
-  'Price5',
-  'Price6',
-  'Price7',
-  'Price8',
-  'Price9',
-  'Price10',
-  'Option1',
-  'Option2',
-  'Option3',
-  'Option4',
-  'Option5',
-  'Option6',
-  'Option7',
-  'Option8',
-  'Option9',
-  'Option10',
-  'Category.Category as Category',
-  'Category.ID as CategoryId',
-  'Subcategory.Subcategory as Subcategory',
-  'Subcategory.ID as SubcategoryId',
-  'CategoryB',
-  'SubCategoryB',
-  'CategoryC',
-  'SubCategoryC',
-  'Keywords',
-]
+  "Thumbnail",
+  "Products.Image as Image",
+  "Hyperlinked_Image as hyperlinkedImage",
+  "SalePrice",
+  "Sale_Start",
+  "Sale_Stop",
+  "Price1",
+  "Price2",
+  "Price3",
+  "Price4",
+  "Price5",
+  "Price6",
+  "Price7",
+  "Price8",
+  "Price9",
+  "Price10",
+  "Option1",
+  "Option2",
+  "Option3",
+  "Option4",
+  "Option5",
+  "Option6",
+  "Option7",
+  "Option8",
+  "Option9",
+  "Option10",
+  "Category.Category as Category",
+  "Category.ID as CategoryId",
+  "Subcategory.Subcategory as Subcategory",
+  "Subcategory.ID as SubcategoryId",
+  "CategoryB",
+  "SubCategoryB",
+  "CategoryC",
+  "SubCategoryC",
+  "Keywords"
+];
 
-module.exports = productFields
+module.exports = productFields;
