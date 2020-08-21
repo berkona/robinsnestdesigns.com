2f45392e06c14da7047c4933f0351b9280021670
rsmonroe <52563866+rsmonroe@users.noreply.github.com>
2020-08-21 07:01:10 -0400

    Update index.js

--- www/content/home/index.js
+++ www/content/home/index.js
@@ -1,14 +1,14 @@
 import featurednew from "./featurednew.html"
-import covid from "./covid.html"
 import featured from "./featured.html"
+import covid from "./covid.html"
 import announcements from "./announcements.html"
 import prairieSchooler from "./prairie-schooler.html"
 import sales from "./sales.html"
 
 export default [
   featurednew,
-  covid,
   featured,
+  covid,
   announcements,
   prairieSchooler,
   sales,
