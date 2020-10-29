0922d58b788ea876adf61655a2b9ae6389f6eb24
rsmonroe <52563866+rsmonroe@users.noreply.github.com>
2020-10-29 06:39:39 -0400

    Update ShippingOptions.js
    Back to all original shipping prices

--- www/components/Checkout/ShippingOptions.js
+++ www/components/Checkout/ShippingOptions.js
@@ -34,8 +34,8 @@ export default ({ shippingAddress, value, setValue }) => {
       label="Free Shipping Over $75"
     />
   </> : <>
-    <FormControlLabel value="16.99" control={<Radio />} label="First Class (International): $16.99" />
-    <FormControlLabel value="36.99" control={<Radio />} label="Priority (International): $36.99" />
+    <FormControlLabel value="14.99" control={<Radio />} label="First Class (International): $14.99" />
+    <FormControlLabel value="34.99" control={<Radio />} label="Priority (International): $34.99" />
   </>
 
   return <>
