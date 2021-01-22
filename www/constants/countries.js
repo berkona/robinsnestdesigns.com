0c00104e7b512d39cc7ec3cc7c3a61abd5111aaf
rsmonroe <52563866+rsmonroe@users.noreply.github.com>
2021-01-22 00:55:59 -0500

    Update countries.js

--- www/constants/countries.js
+++ www/constants/countries.js
@@ -1,19 +1,6 @@
 const countries = [
   { name: "United States of America", value: 'US' },
-  { name: "Australia", value: 'AU' },
-  { name: "Austria", value: 'AT' },
-  { name: "Belgium", value: 'BE' },
   { name: "Canada", value: 'CA' },
-  { name: "Finland", value: 'FI' },
-  { name: "Germany", value: 'DE' },
-  { name: "Japan", value: 'JP' },
-  { name: "Luxembourg", value: 'LU' },
-  { name: "New Zealand", value: 'NZ' },
-  { name: "Norway", value: 'NO' },
   { name: "Puerto Rico", value: 'PR' },
-  { name: "Sweden", value: 'SE' },
-  { name: "Switzerland", value: 'CH' },
-  { name: "The Netherlands", value: 'NL' },
-  { name: "United Kingdom", value: 'GB' },
- ]
+]
 export default countries
